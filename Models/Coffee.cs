23d110064040d049e9e1b8ca776d43e62d555005
vahidkianfar <vahidkianfar@gmail.com>
2022-06-23 15:55:35 +0100

    Added Coffee class.

---
+++ Models/Coffee.cs
@@ -0,0 +1,14 @@
+﻿namespace API_Interactive_Lab_1.Models;
+
+public class Coffee
+{
+    public string Name { get; set; }
+    public string Id { get; set; }
+
+    public Coffee(string name, string id)
+    {
+        Name = name;
+        Id = id;
+    }
+    
+}
